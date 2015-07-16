68425b9990aae6d7b2e67077e8f2035f9b2d136b
Stephen Bussey <steve.bussey@salesloft.com>
2015-07-16 00:20:55 -0400

    Pull all of the non-rspec behavior into a handler

--- lib/rspec-rcv/test_frameworks/rspec.rb
+++ lib/rspec-rcv/test_frameworks/rspec.rb
@@ -5,13 +5,58 @@ module RSpecRcv
     module Metadata
       extend self
 
-      DEFAULTS = {
-          exportable_proc: Proc.new { response.body },
-          export_with: :to_json,
-          fail_on_changed_output: true,
-          export_fixture_to: nil,
-          base_path: nil
-      }
+      class Handler
+        DEFAULTS = {
+            exportable_proc: Proc.new { response.body },
+            export_with: :to_json,
+            fail_on_changed_output: true,
+            export_fixture_to: nil,
+            base_path: nil
+        }
+
+        def initialize(file_path, data, metadata: {})
+          @file_path = file_path
+          @opts = DEFAULTS.merge(metadata)
+          @data = data.call(@opts)
+        end
+
+        def call
+          return if existing_data && existing_data["file"] == file_path && existing_data["data"] == data
+
+          if existing_data && opts[:fail_on_changed_output]
+            if existing_data["data"] != data
+              raise RSpecRcv::DataChangedError.new("Existing data will be overwritten. Turn off this feature with fail_on_changed_output=false")
+            end
+          end
+
+          File.open(path, 'w') do |file|
+            output = { recorded_at: Time.now, file: file_path, data: data }
+            file.write(output.send(opts[:export_with]))
+          end
+        end
+
+        private
+
+        attr_reader :file_path, :data, :opts
+
+        def path
+          if opts[:base_path]
+            File.join(opts[:base_path], opts[:export_fixture_to])
+          else
+            opts[:export_fixture_to]
+          end
+        end
+
+        def existing_data
+          @existing_data ||= if File.exists?(path)
+            begin
+              existing_data = JSON.parse(File.read(path))
+            rescue JSON::ParserError
+              # The file must not have been valid, so we will overwrite it
+            end
+          end
+        end
+      end
 
       def configure!
         ::RSpec.configure do |config|
@@ -19,36 +64,10 @@ module RSpecRcv
 
           config.after(:each, when_tagged_with_rcv) do |ex|
             example = ex.respond_to?(:metadata) ? ex : ex.example
-            opts = DEFAULTS.merge(example.metadata[:rcv])
-            path = if opts[:base_path]
-                     File.join(opts[:base_path], opts[:export_fixture_to])
-                   else
-                     opts[:export_fixture_to]
-                   end
+            opts = example.metadata[:rcv]
+            data_proc = lambda { |opts| instance_eval(&opts[:exportable_proc]) }
 
-            data = instance_eval(&opts[:exportable_proc])
-
-            existing_data = nil
-            if File.exists?(path)
-              begin
-                existing_data = JSON.parse(File.read(path))
-              rescue JSON::ParserError
-                # The file must not have been valid, so we will overwrite it
-              end
-            end
-
-            if existing_data && opts[:fail_on_changed_output]
-              if existing_data["data"] != data
-                raise RSpecRcv::DataChangedError.new("Existing data will be overwritten. Turn off this feature with fail_on_changed_output=false")
-              end
-            end
-
-            unless existing_data && existing_data["file"] == ex.file_path && existing_data["data"] == data
-              File.open(path, 'w') do |file|
-                output = { recorded_at: Time.now, file: ex.file_path, data: data }
-                file.write(output.send(opts[:export_with]))
-              end
-            end
+            Handler.new(ex.file_path, data_proc, metadata: opts).call
           end
         end
       end
